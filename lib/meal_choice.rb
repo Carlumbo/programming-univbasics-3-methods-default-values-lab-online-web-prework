7f9215ec354aa0ba348bc7b42a9800d1b8abce42
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-08 21:08:42 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,21 +4,3 @@ def meal_choice( veg1, veg2, protien = "meat")
   meal = "A plate of #{protien} with #{veg1} and #{veg2}."
   return meal
 end 
-=begin 
-rememeber you can define a variable inside of a method 
-when that varible is called on it will puts out whatever string with interpolation is insde of it
-then you have to call on that varible using puts varible_name and it will put that out
-so example is 
-=end 
-def outfit_choice(color,pants,head = "hat")
-  puts "What are you going to wear?"
-  outfit = "I am going to wear a #{color} shirt with my #{pants} along with a #{head}"
-  puts outfit
-  return outfit
-end 
-=begin
-inside def method 
-define variable 
-puts local_varible 
-return local_varible
-end 
