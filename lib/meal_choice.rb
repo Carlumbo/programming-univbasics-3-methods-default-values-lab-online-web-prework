a0a1b399611b93a58ef1fe0bd3d56bb748c8bfd0
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-08 21:05:55 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,4 +7,11 @@ end
 =begin 
 rememeber you can define a variable inside of a method 
 when that varible is called on it will puts out whatever string with interpolation is insde of it
-=end 
+then you have to call on that varible using puts varible_name and it will put that out
+so example is 
+def outfit_choice(color,pants,head = "hat")
+  puts "What are you going to wear?"
+  outfit = "I am going to wear #{color} shirt with my {pants} along with a #{head}"
+  puts outfit
+  return outfit
+end 
