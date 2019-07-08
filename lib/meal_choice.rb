1b9362d0d785981c32740197aed2ab7f812c2c6e
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-08 21:02:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,10 @@
 # Your code here
 def meal_choice( veg1, veg2, protien = "meat")
   puts "What a nutritious meal!"
-  puts "A plate of #{protien} with #{veg1} and #{veg2}."
-  return "A plate of #{protien} with #{veg1} and #{veg2}."
+  meal = "A plate of #{protien} with #{veg1} and #{veg2}."
+  return meal
 end 
+=begin 
+rememeber you can define a variable inside of a method 
+when that varible is called on it will puts out whatever string with interpolation is insde of it
+=end 
